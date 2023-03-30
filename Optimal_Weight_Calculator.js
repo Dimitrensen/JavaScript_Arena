1875f95e3552b98e2984fe3b8fd8e6647766db0b
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-03-30 15:03:27 +0000

    first prompt not working

--- Optimal_Weight_Calculator.js
+++ Optimal_Weight_Calculator.js
@@ -1,1 +1,8 @@
 //optimal weight for men and women. Equal to their height for men (minus 100), -5 kilos of their height for women
+
+let gender = prompt('What is your sex? (m/f)');
+
+if (gender !== "m" && gender !== "f") {
+    console.log("Invalid parameter");
+    process.exit(0);
+}
