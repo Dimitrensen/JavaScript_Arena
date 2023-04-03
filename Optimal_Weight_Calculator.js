2233f7ec761736943b8e9bda58488bb5eb3bdf9d
Dimitris Alexiou <97767993+Dimitrensen@users.noreply.github.com>
2023-04-03 11:42:14 +0000

    syncreadline used

--- Optimal_Weight_Calculator.js
+++ Optimal_Weight_Calculator.js
@@ -1,8 +1,21 @@
 //optimal weight for men and women. Equal to their height for men (minus 100), -5 kilos of their height for women
 
-let gender = prompt('What is your sex? (m/f)');
+const readlineSync = require('readline-sync');
+
+let gender = readlineSync.question('What is your sex? (m/f) ');
 
 if (gender !== "m" && gender !== "f") {
     console.log("Invalid parameter");
     process.exit(0);
 }
+
+let height = readlineSync.question('What is your height? (in cm) ');
+
+height = parseInt(height);
+
+if (isNaN(height)) {
+    console.log('Invalid input');
+    process.exit(0);
+}
+
+console.log('Your height is ' + height + 'cm')
